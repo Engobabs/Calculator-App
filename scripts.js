4a7b82343614eb136330603490581e1081a0f9e6
Engobabs <engobabs@gmail.com>
2025-05-21 15:33:48 +0100

    Organised the JavaScript code into a nice, readable and understable structure

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 'use strict'
 
+/////////////////////////// Declaration of Variables //////////////////////////////////////
 const calculatorDisplay = document.querySelector('h1');
 const inputButtons = document.querySelectorAll('button');
 const clearButton = document.getElementById('clear-btn');
@@ -9,37 +10,7 @@ let operatorValue = '';
 let awaitingNextValue = false;
 
 
-// Functions
-function sendNumberValue(number) {
-    // Replace cuurnet display value if first value is entered
-    if (awaitingNextValue) {
-        calculatorDisplay.textContent = number;
-        awaitingNextValue = false
-    }
-    else {
-        // If the current display value is 0, replace it, if not add number
-        const displayValue = calculatorDisplay.textContent;
-        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
-    }
-}
-
-// Reset Display
-function resetAll() {
-    calculatorDisplay.textContent = '0'
-    firstValue = 0;
-    operatorValue = '';
-    awaitingNextValue = false;
-}
-
-function addDecimal() {
-    // if operator pressed, don't add decimal
-    if (awaitingNextValue) return;
-    // if no decimal, add one
-    if (!calculatorDisplay.textContent.includes('.')) {
-        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
-    }
-}
-
+////////////////////////////////// Objects //////////////////////////////////////////////
 // Calculate first and second value depending on operator
 const calculate = {
     '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
@@ -53,6 +24,23 @@ const calculate = {
     '=': (firstNumber, secondNumber) => secondNumber,
 };
 
+
+//////////////////////////////// Functions ///////////////////////////////////////////////
+// Input number function
+function sendNumberValue(number) {
+    // Replace cuurnet display value if first value is entered
+    if (awaitingNextValue) {
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false
+    }
+    else {
+        // If the current display value is 0, replace it, if not add number
+        const displayValue = calculatorDisplay.textContent;
+        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
+}
+
+// Use Operator function
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
     // Prevent multiple operators
@@ -71,12 +59,28 @@ function useOperator(operator) {
     // Ready for next value, store operator
     awaitingNextValue = true;
     operatorValue = operator;
+}
 
+// Add Decimal Function
+function addDecimal() {
+    // if operator pressed, don't add decimal
+    if (awaitingNextValue) return;
+    // if no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
+
+// Reset Display
+function resetAll() {
+    calculatorDisplay.textContent = '0'
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
 }
 
 
-
-// Event Listeners
+////////////////////////////// Event Listeners ///////////////////////////////////////
 inputButtons.forEach((inputButton) => {
     if (inputButton.classList.length === 0) {
         inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
